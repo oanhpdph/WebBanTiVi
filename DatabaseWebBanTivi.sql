821f15a177a21686904eec424c553dbd89afff0a
PHAMDUCOANH\FPTSHOP <oanhpdph25707@fpt.edu.vn>
2023-07-08 09:12:03 +0700

    no message

--- DatabaseWebBanTivi.sql
+++ DatabaseWebBanTivi.sql
@@ -1,17 +1,19 @@
 ﻿CREATE DATABASE webbantivi
+
 go
 
 USE webbantivi
+
 go
 
 -- lý do trả hàng
 CREATE TABLE reason
   (
      id     INT IDENTITY PRIMARY KEY,
-     reason NVARCHAR(max) -- lý do trả hàng là ?
+     reason NVARCHAR(max)-- lý do trả hàng là ?
   )
 
-  -- sản phẩm
+-- sản phẩm
 CREATE TABLE products
   (
      id BIGINT IDENTITY(1, 1) PRIMARY KEY
@@ -28,34 +30,33 @@ CREATE TABLE bill
   (
      id BIGINT IDENTITY(1, 1) PRIMARY KEY
   )
-  
 
 -- phiếu trả hàng
 CREATE TABLE returns_product
   (
      id          BIGINT IDENTITY(1, 1) PRIMARY KEY,
-     id_customer BIGINT references customer(id),
-     id_bill     BIGINT  references bill(id),
-     id_product  BIGINT  references products(id),
+     id_customer BIGINT REFERENCES customer(id),
+     id_bill     BIGINT REFERENCES bill(id),
+     id_product  BIGINT REFERENCES products(id),
      create_date DATETIME,
      quantity    INT,
      status      INT -- trạng thái phiếu trả hàng
   )
 
-  -- chi tiết lý do trả hàng
-  CREATE TABLE returns_reason
+-- chi tiết lý do trả hàng
+CREATE TABLE returns_reason
   (
-     id_reason  int references reason(id),
-     id_returns BIGINT  references returns_product(id),
-     note       NVARCHAR(200) ,
-	 primary key (id_reason, id_returns)
-  ) 
+     id_reason  INT REFERENCES reason(id),
+     id_returns BIGINT REFERENCES returns_product(id),
+     note       NVARCHAR(200),
+     PRIMARY KEY (id_reason, id_returns)
+  )
 
 -- ảnh hoặc video đính kèm trả hàng
 CREATE TABLE image_or_video
   (
      id             UNIQUEIDENTIFIER DEFAULT Newid() PRIMARY KEY,
-     id_returns     BigInt REFERENCES returns_product(id) ,-- id phiếu trả hàng
+     id_returns     BIGINT REFERENCES returns_product(id),
+     -- id phiếu trả hàng
      image_or_video VARCHAR(max)
-  )
-
+  ) 
