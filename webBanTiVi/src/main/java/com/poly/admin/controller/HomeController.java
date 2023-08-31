c4a8f5a0b5d9d853762dc3cb62b85bc166079d78
PHAMDUCOANH\FPTSHOP <oanhpdph25707@fpt.edu.vn>
2023-08-31 21:27:44 +0700

    oanh up code bán hàng tại quầy

---
+++ webBanTiVi/src/main/java/com/poly/admin/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.poly.admin.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/admin")
+    public String loadHome(HttpSession session){
+        session.setAttribute("pageView","/admin/page/dashboard.html");
+        return "admin/layout";
+    }
+
+    @GetMapping("/admin/dashboard")
+    public String loadDashboard(HttpSession session){
+    session.setAttribute("pageView","/admin/page/dashboard.html");
+        return "admin/layout";
+    }
+    @GetMapping("/admin/bill")
+    public String loadBill(HttpSession session){
+        session.setAttribute("pageView","/admin/page/bill.html");
+        return "admin/layout";
+    }
+    @GetMapping("/admin/direct_sales")
+    public String loadDirectSales(HttpSession session){
+        session.setAttribute("pageView","/admin/page/directSales.html");
+        return "admin/layout";
+    }
+    @GetMapping("/admin/customer")
+    public String loadCustomer(HttpSession session){
+        session.setAttribute("pageView","/admin/page/customer.html");
+        return "admin/layout";
+    }
+    @GetMapping("/admin/staff")
+    public String loadStaff(HttpSession session){
+        session.setAttribute("pageView","/admin/page/staff.html");
+        return "admin/layout";
+    }
+}
