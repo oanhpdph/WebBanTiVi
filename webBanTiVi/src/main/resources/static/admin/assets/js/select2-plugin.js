969eeb3e0274bf6e2ade6eae99a52a1732900bd2
PHAMDUCOANH\FPTSHOP <oanhpdph25707@fpt.edu.vn>
2023-09-05 21:40:52 +0700

    oanh sửa lỗi select2 không hiện option

--- webBanTiVi/src/main/resources/static/admin/assets/js/select2-plugin.js
+++ webBanTiVi/src/main/resources/static/admin/assets/js/select2-plugin.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
+    $('.select2').select2({
+        minimumResultsForSearch:5
+    });
     $('.select2-city').select2({
         placeholder: '---Click to select---',
         minimumResultsForSearch:10,
-        allowClear:true,
+        allowClear:true
     });
-    $('.select2-gender').select2({
-        minimumResultsForSearch:Infinity
-    });
-    $('.select2').select2({
-        minimumResultsForSearch:5,
-    });
+
 });
