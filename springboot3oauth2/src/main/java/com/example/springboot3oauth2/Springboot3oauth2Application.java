0179f92b71f9274260af59c3ec9c15e0868c6a26
HELLOMANH\TGDD <manhbdph25669@fpt.edu.vn>
2023-09-12 21:52:38 +0700

    manh up code order user

---
+++ springboot3oauth2/src/main/java/com/example/springboot3oauth2/Springboot3oauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot3oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3oauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3oauth2Application.class, args);
+    }
+
+}
---
+++ springboot3oauth2/src/test/java/com/example/springboot3oauth2/Springboot3oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3oauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
