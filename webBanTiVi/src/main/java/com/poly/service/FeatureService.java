bb72cc16fdf95c3eee339c43e16704bc3f922d27
Thái Nam <phamngocthainamtb7@gmail.com>
2023-10-27 19:59:04 +0700

    crud admin coupon,feature

--- webBanTiVi/src/main/java/com/poly/service/FeatureService.java
+++ webBanTiVi/src/main/java/com/poly/service/FeatureService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface FeatureService {
     Feature save(Feature feature);
 
-    void delete(Integer id);
+    void deleteById(Integer id);
 
     List<Feature> findAll();
 
