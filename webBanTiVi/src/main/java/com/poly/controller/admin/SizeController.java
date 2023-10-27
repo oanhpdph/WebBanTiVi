ebe3ec6e44443dc33d5c2a91775051365f479e70
AnhDungdevhihi <dungcanxaha3@gmail.com>
2023-10-27 22:26:57 +0700

    crud size,resolution,color,brand,manufacture

--- webBanTiVi/src/main/java/com/poly/controller/admin/SizeController.java
+++ webBanTiVi/src/main/java/com/poly/controller/admin/SizeController.java
@@ -1,48 +1,55 @@
 package com.poly.controller.admin;
 
 
+import com.poly.entity.Color;
 import com.poly.entity.Size;
 
 import com.poly.service.Impl.SizeServiceImpl;
 import com.poly.service.SizeService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/size")
+@Controller
+@RequestMapping("/admin")
 public class SizeController {
     @Autowired
     SizeServiceImpl sizeService;
 
+    @GetMapping("/technical/size")
+    public String loadSize(HttpSession session, Model model) {
+        session.setAttribute("pageView", "/admin/page/technical/size.html");
+        session.setAttribute("active", "/technical/size");
+        model.addAttribute("size", new Size());
+        model.addAttribute("listSize", this.sizeService.getAll());
+        return "admin/layout";
+    }
 
-
-    @PostMapping("/add")
-    public ResponseEntity<?> add(@RequestBody @Valid Size size, BindingResult result){
-        if(result.hasErrors()){
-            return new ResponseEntity<>(result.getAllErrors(), HttpStatus.BAD_REQUEST);
+    @PostMapping("/technical/size/add")
+    public String addColor(Model model, @Valid @ModelAttribute("size") Size size, BindingResult bindingResult)  {
+        if (bindingResult.hasErrors()) {
+            return "admin/layout";
         }
-        return new ResponseEntity<>(sizeService.add(size), HttpStatus.OK);
+        this.sizeService.add(size);
+        model.addAttribute("listSize", sizeService.getAll());
+        return "redirect:/admin/technical/size";
     }
 
 
-
-
-    @DeleteMapping("/delete/{id}")
-    public void delete(@PathVariable Integer id){
-
+    @GetMapping("/technical/size/delete/{id}")
+    public String deleteColor(@PathVariable("id") Integer id, Model model) {
+        Size size = sizeService.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         sizeService.delete(id);
-    }
-
-    @PutMapping("/update")
-    public Size update(@RequestBody Size size){
-
-        return sizeService.add(size);
+        return "redirect:/admin/technical/size";
     }
 
 }
--- webBanTiVi/src/main/java/com/poly/service/Impl/SizeServiceImpl.java
+++ webBanTiVi/src/main/java/com/poly/service/Impl/SizeServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SizeServiceImpl implements SizeService {
@@ -28,5 +29,10 @@ public class SizeServiceImpl implements SizeService {
     @Override
     public List<Size> getAll() {
         return sizeRepository.findAll();
+    }
+
+    @Override
+    public Optional<Size> findById(Integer id) {
+        return this.sizeRepository.findById(id);
     }
 }
