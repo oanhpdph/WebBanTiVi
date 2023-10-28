bcba4ac13a52d74cc3bd194efc6716dec3991886
Thái Nam <phamngocthainamtb7@gmail.com>
2023-10-28 22:11:01 +0700

    sua?

--- webBanTiVi/src/main/java/com/poly/entity/Coupon.java
+++ webBanTiVi/src/main/java/com/poly/entity/Coupon.java
@@ -1,10 +1,7 @@
 package com.poly.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "coupon")
@@ -12,6 +9,8 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Builder
 public class Coupon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
