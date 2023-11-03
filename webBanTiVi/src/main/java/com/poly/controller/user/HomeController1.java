776df5fafb128cf22786361f263d33b86a3b0e34
PhamDucOanh <114309807+oanhpdph@users.noreply.github.com>
2023-11-03 23:39:00 +0700

    Update HomeController1.java

--- webBanTiVi/src/main/java/com/poly/controller/user/HomeController1.java
+++ webBanTiVi/src/main/java/com/poly/controller/user/HomeController1.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController1 {
+public class HomeController1 { 
 
     @GetMapping("/")
     public String loadHome(HttpSession session){
