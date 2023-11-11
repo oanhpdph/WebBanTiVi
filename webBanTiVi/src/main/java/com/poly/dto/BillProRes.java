32b17267d9936431d51580410033899f8e4214a3
AHIHI\admin <tuvaph25694@fpt.edu.vn>
2023-11-12 01:14:23 +0700

    update pay

--- webBanTiVi/src/main/java/com/poly/dto/BillProRes.java
+++ webBanTiVi/src/main/java/com/poly/dto/BillProRes.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class BillProRes {
 
     List<Integer> product;
-    List<Integer> quantity;
+    Integer quantity;
     String name;
     String address;
     String phoneNumber;
